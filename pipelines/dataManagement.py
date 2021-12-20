cb612d82b8ca7095bb989777ecb692facc54ed84
Atellas23 <alex.batlle01@gmail.com>
2021-12-20 10:02:11 +0100

    update

--- pipelines/dataManagement.py
+++ pipelines/dataManagement.py
@@ -11,30 +11,6 @@ from datetime import datetime as dt
 import datetime
 
 
-# # Set constants to point to the utility files and paths used
-# HADOOP_HOME = "./resources/hadoop_home"
-# JDBC_JAR = "./resources/postgresql-42.2.8.jar"
-# PYSPARK_PYTHON = "python3.8"
-# PYSPARK_DRIVER_PYTHON = "python3.8"
-
-# # Set the corresponding environment variables
-# os.environ["HADOOP_HOME"] = HADOOP_HOME
-# sys.path.append(HADOOP_HOME + "/bin")
-# os.environ["PYSPARK_PYTHON"] = PYSPARK_PYTHON
-# os.environ["PYSPARK_DRIVER_PYTHON"] = PYSPARK_DRIVER_PYTHON
-
-# # Create the configuration
-# conf = SparkConf()
-# conf.set("spark.jars", JDBC_JAR)
-
-# # Create the spark session with the JDBC driver
-# spark = SparkSession.builder \
-#     .config(conf=conf) \
-#     .master("local") \
-#     .appName("testing") \
-#     .getOrCreate()
-
-
 def process(spark_context: pyspark.SparkContext, spark_session: SparkSession):
     # Read the user credentials to access the database in PostgresFIB
     with open('.uname_and_pwd', 'r') as file:
@@ -69,16 +45,16 @@ def process(spark_context: pyspark.SparkContext, spark_session: SparkSession):
                                   .option("user", username)
                                   .option("password", password)
                                   .load()
-                                  .select('aircraftregistration', 'starttime', 'kind')
+                                  .select('aircraftregistration', 'starttime', 'subsystem', 'kind')
                                   .rdd
-                                  .map(lambda t: ((parse_date(str(t[1])[:19], '%Y-%m-%d %H:%M:%S'), str(t[0])), 1, t[-1]))
+                                  .filter(lambda t: t[-2] == '3453')
+                                  .map(lambda t: ((parse_date(str(t[1])[:19], '%Y-%m-%d %H:%M:%S'), str(t[0])), t[-1]))
                                   .filter(lambda t: t[-1] in ('Delay', 'Safety', 'AircraftOnGround'))
                                   .map(lambda t: t[:-1])
                                   )
 
     # Read the files in the resources/trainingData directory and
     # obtain the mean of the sensor data per aircraft per day
-    # sc = pyspark.SparkContext.getOrCreate()
     csv_files = spark_context.wholeTextFiles('resources/trainingData/*')\
         .map(lambda t: (t[0][-30:-4], t[1]))\
         .map(lambda t: ((parse_date(t[0][:6], '%d%m%y'), t[0][-6:]), t[1]))\
@@ -93,25 +69,16 @@ def process(spark_context: pyspark.SparkContext, spark_session: SparkSession):
     data = aircraft_utilization_rdd.join(
         csv_files).mapValues(lambda v: (*v[0], v[1]))
 
-    # Aquest tros no funciona
-    ##
+    operation_interruption_rdd = operation_interruption_rdd.flatMap(lambda t: [((str(dt.strptime(
+        t[0][0], '%Y-%m-%d').date()-datetime.timedelta(days=d)), t[0][1]), True) for d in range(7)])
 
-    def any_maintenances_in_post_7_days(aircraftid: str, timeid: str, format: str = '%Y-%m-%d'):
-        for d in range(7):
-            if operation_interruption_rdd.filter(lambda t: t[0][0] == str(dt.strptime(timeid, format).date()+datetime.timedelta(days=d)) and t[0][1] == aircraftid).count() > 0:
-                return True
-    # Bàsicament, el problema és que no es poden fer transformacions dins de transformacions
-    # (filter inside of map)
-    data = data.map(lambda t: (
-        t[0], any_maintenances_in_post_7_days(t[0][1], t[0][0]), *t[1]))
-    ##
-    print(data.first())
-    return None
+    data = data.leftOuterJoin(operation_interruption_rdd).mapValues(
+        lambda v: (*v[0], v[1])).mapValues(lambda v: (*v[:-1], 'Maintenance' if v[-1] is not None else 'NoMaintenance'))
 
     # Return the matrix (save it?)
     with open('result.csv', 'w') as f:
         data = data.values().collect()
-        result = 'FH,FC,DM,SensorAVG,Count\n'
+        result = 'FH,FC,DM,SensorAVG,Label\n'
         for v in data:
             aux = map(str, v)
             result += ','.join(aux)+'\n'
--- pipelines/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-for csv_file in csv_files:
-#     data = csv_file.mapValues(lambda v: v.split(
-#         '\n')).filter(lambda t: 'date' not in t[0])
-#     print(data.first())
-#     break
