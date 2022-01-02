e57491913bbad8dc71b8677687c51cec8c8aac90
Atellas23 <alex.batlle01@gmail.com>
2022-01-02 18:11:15 +0100

    Removed useless files/lines from `main.py`.

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from pyspark.sql import SparkSession
 from pipelines.dataManagement import process as data_management
 from pipelines.dataAnalysis import process as data_analysis
 from pipelines.runtimeClassifier import process as runtime_classifier
-from test import process as process_test
 
 HADOOP_HOME = "./resources/hadoop_home"
 JDBC_JAR = "./resources/postgresql-42.2.8.jar"
@@ -14,15 +13,12 @@ PYSPARK_PYTHON = "python3.8"
 PYSPARK_DRIVER_PYTHON = "python3.8"
 
 if(__name__ == "__main__"):
-    # if len(sys.argv) != 2:
-    #     print(f'usage: {sys.argv[0]} (dm, da, rc)')
-    #     exit()
     os.environ["HADOOP_HOME"] = HADOOP_HOME
     sys.path.append(HADOOP_HOME + "\\bin")
     os.environ["PYSPARK_PYTHON"] = PYSPARK_PYTHON
     os.environ["PYSPARK_DRIVER_PYTHON"] = PYSPARK_DRIVER_PYTHON
 
-    conf = SparkConf()  # create the configuration
+    conf = SparkConf()  # Create the configuration
     conf.set("spark.jars", JDBC_JAR)
 
     spark = SparkSession.builder \
